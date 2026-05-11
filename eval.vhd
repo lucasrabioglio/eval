24ffd75246e0207eaa7c06433f99620316e567a7
Lucas <lucasrabioglio@gmail.com>
2026-05-11 20:01:11 -0300

    Agregado de registro para aumentar velocidad, pero aumenta latencia.

--- eval.vhd
+++ eval.vhd
@@ -10,6 +10,7 @@ generic(Nbits : natural := 18;   -- Estos 18 bits son de multiplicar 9bits*512, 
 port(	ECDF_exp : in  std_logic_vector(Nbits - 1 downto 0);
 		ECDF_teo : in  std_logic_vector(Nbits - 1 downto 0);
 		clk, rst : in  std_logic;
+			SAD_1 : out std_logic_vector(Nbits - 1 downto 0);
 		     SAD : out std_logic_vector(Nbits - 1 downto 0)
 		);
 		
@@ -17,22 +18,34 @@ end entity;
 
 architecture rtl of eval is
 	
-	signal error_reg : std_logic_vector(Nbits - 1 downto 0);
-	signal 	  error : std_logic_vector(Nbits - 1 downto 0);
+	signal abs_reg,error_reg : std_logic_vector(Nbits - 1 downto 0);
+	signal resta,acum : std_logic_vector(Nbits - 1 downto 0);
 	
 begin
 
-	error <= std_logic_vector(abs(signed(ECDF_exp) - signed(ECDF_teo)) + signed(error_reg));
+	resta <= std_logic_vector(abs(signed(ECDF_exp) - signed(ECDF_teo)));
 	
-	process(ECDF_exp,ECDF_teo)
+	process(rst,clk)
+	begin
+		if rst = '0' then
+			abs_reg <= (others => '0');
+		elsif rising_edge(clk) then
+			abs_reg <= resta;
+		end if;
+	end process;
+	
+	process(rst,clk)
 	begin
 		if rst = '0' then
 			error_reg <= (others => '0');
 		elsif rising_edge(clk) then
-			error_reg <= error;
+			error_reg <= acum;
 		end if;
 	end process;
 	
+	acum <= std_logic_vector(unsigned(error_reg) + unsigned(abs_reg));
+	
 	SAD <= error_reg;
+	SAD_1 <= acum;
 	
 end rtl;
